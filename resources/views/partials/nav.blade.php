049aa2dae0b993e53284475dc8d53e698d99e8a8
Akimius <akim.savchenko@gmail.com>
2017-11-15 23:34:02 +0200

    first commit

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#myPage">Logo</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#about">О НАС</a></li>
+                <li><a href="#services">СЕРВИСЫ</a></li>
+                <li><a href="#portfolio">ПОРТФОЛИО</a></li>
+                <li><a href="#pricing">ЦЕНЫ</a></li>
+                <li><a href="#contact">КОНТАКТЫ</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="jumbotron text-center">
+    <h1>Компания</h1>
+    <p>Мы специализируемся на ... </p>
+</div>
